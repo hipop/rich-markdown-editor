45a4ed1b8287178552490a5e8232ff8e7cd9c31d
tecc <tecc@tecc.me>
2022-10-15 03:35:58 +0200

    change: Fix import error

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -2,7 +2,8 @@ import Editor from "./index";
 import debounce from "lodash/debounce";
 import { Props } from "..";
 import React from "react";
-import { Story, Meta } from "@storybook/react/types-6-0";
+// eslint-disable-next-line prettier/prettier
+import type { Story, Meta } from "@storybook/react/types-6-0";
 
 export default {
   title: "Editor",
